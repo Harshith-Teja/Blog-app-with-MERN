d98caef8749f9ec9e1597b3a54c8809087844cb2
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-10 12:32:36 +0530

    added corsOptions to allow FE req's at BE

---
+++ backend/src/config/corsOptions.ts
@@ -0,0 +1,15 @@
+import allowedOrigins from "./allowedOrigins";
+
+const corsOptions = {
+  origin: (
+    origin: string | null,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (allowedOrigins.indexOf(origin || "") != -1 || !origin)
+      callback(null, true);
+    else callback(new Error("Not allowed by cors"));
+  },
+  optionsSuccessStatus: 200,
+};
+
+export default corsOptions;
