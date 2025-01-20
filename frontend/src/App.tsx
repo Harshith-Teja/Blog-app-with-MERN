7ffc1ca5994e4c93691a5d6f20e9821c6afe2271
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-20 09:49:23 +0530

    added various pages to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
+import Home from "./components/Home";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Blogs from "./components/Blogs";
+import Layout from "./components/Layout";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
     <div className="w-full h-dvh">
       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Route>
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
