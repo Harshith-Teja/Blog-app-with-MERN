a9c0a98429344633c5e5b081d421a195758062ce
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-23 11:02:47 +0530

    removed AuthProvider with respect to AuthContext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </Router>
-      </Provider>
-    </AuthProvider>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </StrictMode>
 );
