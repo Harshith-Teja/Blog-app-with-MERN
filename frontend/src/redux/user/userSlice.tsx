0bae954145f08ec724863ba48e829b40820c1d92
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-25 17:37:47 +0530

    added update user actions

--- frontend/src/redux/user/userSlice.tsx
+++ frontend/src/redux/user/userSlice.tsx
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type User = {
+  _id?: string;
   uname: string;
   pwd: string;
   refreshToken?: string[];
@@ -37,9 +38,29 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
