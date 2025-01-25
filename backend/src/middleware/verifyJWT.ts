3b1f9187dbcbfffc30e572718a33500ccbc2245c
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-25 17:39:18 +0530

    re-modified the verifyJWT logic

--- backend/src/middleware/verifyJWT.ts
+++ backend/src/middleware/verifyJWT.ts
@@ -4,32 +4,24 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 declare global {
   namespace Express {
     interface Request {
-      user?: string; //uname extracted from JWT
+      userId?: string; //uname extracted from JWT
     }
   }
 }
 
-interface DecodedToken extends JwtPayload {
-  username: string;
-}
-
 export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader: string | undefined = req.headers.authorization;
+  const token: string | undefined = req.cookies?.jwt;
 
-  if (!authHeader?.startsWith("Bearer "))
-    return res.status(401).json({ message: "Auth header doesn't exist" });
-
-  const accessToken = authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   jwt.verify(
-    accessToken,
-    process.env.ACCESS_TOKEN_SECRET as string,
+    token,
+    process.env.REFRESH_TOKEN_SECRET as string,
     (err, decoded) => {
       if (err) return res.status(403).json({ message: err.message });
 
-      const payload = decoded as DecodedToken;
-
-      req.user = payload?.username;
+      const payload = decoded as { id: string };
+      req.userId = payload.id;
 
       next();
     }
