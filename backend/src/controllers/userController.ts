afb31ea4c7e62a7e2ef2da4aa2fe5430fe32c42c
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-25 17:39:45 +0530

    added updateUser req handler

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,40 @@
 import { Request, Response } from "express";
 import { IUser, User } from "../models/users";
+import bcrypt from "bcrypt";
+
+export const updateUser = async (req: Request, res: Response) => {
+  if (req.userId !== req.params.id)
+    return res.status(401).json({ message: "Unauthorized" });
+
+  if (!req.body.uname && !req.body.pwd)
+    return res.status(400).json({ message: "Nothing to update" });
+
+  const updateFields: Partial<IUser> = {};
+
+  if (req.body.uname) updateFields.uname = req.body.uname;
+
+  if (req.body.pwd) updateFields.pwd = await bcrypt.hash(req.body.pwd, 10);
+
+  try {
+    const updatedUser: IUser | null = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: updateFields,
+      },
+      { new: true, runValidators: false }
+    );
+
+    //check if user exists
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+
+    const { pwd, ...userWithoutPwd } = updatedUser?.toObject();
+
+    res.status(200).json({ userWithoutPwd });
+  } catch (err: any) {
+    res.status(500).json({ message: "An unexpected error occured" });
+  }
+};
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
