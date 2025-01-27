09eb4efd7ad10959d15332f88b0c2d6f701de1aa
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-27 17:54:09 +0530

    buitl createPost functionality

--- frontend/src/components/CreatePost.tsx
+++ frontend/src/components/CreatePost.tsx
@@ -1,12 +1,46 @@
-import { Button, Select, TextInput } from "flowbite-react";
+import axios from "axios";
+import { Alert, Button, Select, TextInput } from "flowbite-react";
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [formData, setFormData] = useState({});
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/create-post",
+        JSON.stringify(formData),
+        {
+          headers: { "Content-Type": "Application/json" },
+          withCredentials: true,
+        }
+      );
+
+      const data = response.data;
+      console.log(data);
+
+      if (data.success === false) {
+        setErrMsg(data.message);
+        return;
+      }
+
+      setErrMsg("");
+      navigate(`/posts/${data.savedPost.slug}`);
+    } catch (err: any) {
+      setErrMsg(err.message);
+    }
+  };
   return (
     <div className="p-4 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-4xl font-bold my-7">Create a post</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
             type="text"
@@ -14,8 +48,15 @@ const CreatePost = () => {
             placeholder="Title"
             className="flex-1"
             required
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
-          <Select>
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <option value="uncategorized">Select a category</option>
             <option value="programming">Programming</option>
             <option value="travel">Travel</option>
@@ -26,10 +67,16 @@ const CreatePost = () => {
           theme="snow"
           placeholder="Write something"
           className="h-72 mb-12"
+          onChange={(value) => setFormData({ ...formData, content: value })}
         />
         <Button type="submit" gradientDuoTone="purpleToBlue">
           Publish
         </Button>
+        {errMsg && (
+          <Alert color="failure" className="mt-2">
+            {errMsg}
+          </Alert>
+        )}
       </form>
     </div>
   );
