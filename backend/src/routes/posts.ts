96761c2d723b7629eb73fb3590d7a34f2984113b
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-28 11:12:34 +0530

    added delete post route

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,9 +1,14 @@
 import express, { RequestHandler } from "express";
-import { createPost, getPosts } from "../controllers/postController";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+} from "../controllers/postController";
 
 const router = express.Router();
 
 router.post("/create-post", createPost as RequestHandler);
 router.get("/get-posts", getPosts);
+router.delete("/delete-post/:postId/:userId", deletePost as RequestHandler);
 
 export default router;
