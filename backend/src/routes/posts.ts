5c098c4d0140278772f5aab99c9460abe81e1fbb
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-28 12:26:11 +0530

    added update-post route

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -3,6 +3,7 @@ import {
   createPost,
   deletePost,
   getPosts,
+  updatePost,
 } from "../controllers/postController";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/create-post", createPost as RequestHandler);
 router.get("/get-posts", getPosts);
 router.delete("/delete-post/:postId/:userId", deletePost as RequestHandler);
+router.put("/update-post/:postId/:userId", updatePost as RequestHandler);
 
 export default router;
