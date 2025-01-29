a18c4c5b090026eb549a501cd9f3af6e9c86ee33
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-29 19:11:52 +0530

    added route of like-comment

--- backend/src/routes/comment.ts
+++ backend/src/routes/comment.ts
@@ -2,11 +2,13 @@ import express, { RequestHandler } from "express";
 import {
   createComment,
   getPostComments,
+  likeComment,
 } from "../controllers/commentController";
 
 const router = express.Router();
 
 router.post("/create-comment", createComment as RequestHandler);
 router.get("/get-post-comments/:postId", getPostComments as RequestHandler);
+router.put("/like-comment/:commentId", likeComment as RequestHandler);
 
 export default router;
