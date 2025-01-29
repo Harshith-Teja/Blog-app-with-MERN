8c68862d44f017959ef1f1ad1d33aac2c4ecf131
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-29 17:23:37 +0530

    added getPostComments route

--- backend/src/routes/comment.ts
+++ backend/src/routes/comment.ts
@@ -1,8 +1,12 @@
 import express, { RequestHandler } from "express";
-import { createComment } from "../controllers/commentController";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/commentController";
 
 const router = express.Router();
 
 router.post("/create-comment", createComment as RequestHandler);
+router.get("/get-post-comments/:postId", getPostComments as RequestHandler);
 
 export default router;
