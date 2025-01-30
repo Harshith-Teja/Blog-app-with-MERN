a670128225207cad06f2734fcde6e1178b353480
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-30 10:06:49 +0530

    added edit comment route

--- backend/src/routes/comment.ts
+++ backend/src/routes/comment.ts
@@ -1,6 +1,7 @@
 import express, { RequestHandler } from "express";
 import {
   createComment,
+  editComment,
   getPostComments,
   likeComment,
 } from "../controllers/commentController";
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/create-comment", createComment as RequestHandler);
 router.get("/get-post-comments/:postId", getPostComments as RequestHandler);
 router.put("/like-comment/:commentId", likeComment as RequestHandler);
+router.put("/edit-comment/:commentId", editComment as RequestHandler);
 
 export default router;
