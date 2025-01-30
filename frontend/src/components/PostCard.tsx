9735f6b176f03b20e00ebddf0f64e102fae51c57
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-30 13:01:56 +0530

    added PostCard component to display recent articles

---
+++ frontend/src/components/PostCard.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+type PostType = {
+  _id: string;
+  title: string;
+  content: string;
+  category: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type PostCardProps = {
+  post: PostType;
+};
+
+const PostCard = ({ post }: PostCardProps) => {
+  return (
+    <div className="p-3 flex-1 flex flex-col gap-2 border hover:border-2 border-slate-500 rounded-lg hover:scale-105 transition-transform duration-300">
+      <p className="text-lg font-semibold">{post.title}</p>
+      <span className="italic text-sm">
+        {post.category !== "uncategorized" && post.category}
+      </span>
+      <Link
+        to={`/posts/${post.slug}`}
+        className="border border-teal-400 text-teal-400 p-2 m-2 rounded-md mt-auto"
+      >
+        Read article
+      </Link>
+    </div>
+  );
+};
+
+export default PostCard;
