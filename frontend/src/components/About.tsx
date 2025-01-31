6a5a52dcecab23505ad087f20831f3b782f9f5ca
Harshith Teja <harshithtejadontoju@gmail.com>
2025-01-31 17:27:37 +0530

    built about page

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,5 +1,35 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <section className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Blog Smith
+          </h1>
+          <div className="text-lg text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum asperiores voluptate esse aspernatur consequatur
+              expedita qui perspiciatis architecto sequi doloremque ut inventore
+              quo necessitatibus a, veniam saepe placeat aliquam. Aspernatur?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum asperiores voluptate esse aspernatur consequatur
+              expedita qui perspiciatis architecto sequi doloremque ut inventore
+              quo necessitatibus a, veniam saepe placeat aliquam. Aspernatur?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum asperiores voluptate esse aspernatur consequatur
+              expedita qui perspiciatis architecto sequi doloremque ut inventore
+              quo necessitatibus a, veniam saepe placeat aliquam. Aspernatur?
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
