76edb8df06e27e4ce6de5d1c607d67b23a8959c4
Harshith Teja <harshithtejadontoju@gmail.com>
2025-02-01 19:11:15 +0530

    added frontend render url into allowedOrigins

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blog-smith-fe.onrender.com",
+];
 
 export default allowedOrigins;
