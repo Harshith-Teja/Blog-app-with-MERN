c55202228d04df8e3bdd71d49e01f00764ef38bb
Harshith Teja <harshithtejadontoju@gmail.com>
2025-02-02 09:02:25 +0530

    logging origin for debugging purpose

--- backend/src/middleware/credentials.ts
+++ backend/src/middleware/credentials.ts
@@ -4,7 +4,7 @@ import allowedOrigins from "../config/allowedOrigins";
 //allows the client to send cookies to the server(backend)
 const credentials = (req: Request, res: Response, next: NextFunction) => {
   const origin = req.headers.origin;
-
+  console.log("origin ", origin);
   if (allowedOrigins.includes(origin || "")) {
     res.header("Access-Control-Allow-Credentials", "true");
   }
