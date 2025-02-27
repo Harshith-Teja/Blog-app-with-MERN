3ae4f7568b7b67ea7603a346643383248dbb0ebe
Harshith Teja <harshithtejadontoju@gmail.com>
2025-02-27 21:58:58 +0530

    added custom fetchPosts hook

---
+++ frontend/src/hooks/fetch/useFetchPosts.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetchPosts = (url: string) => {
+  const [data, setData] = useState({ posts: [], totalPosts: 0 });
+  const [postsLoading, setPostsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setPostsLoading(true);
+      try {
+        const response = await axios.get(url, {
+          withCredentials: true,
+        });
+
+        const data = response.data;
+
+        if (data.success === false) {
+          setPostsLoading(false);
+          console.log(data.message);
+          return;
+        }
+
+        setData(data);
+        setPostsLoading(false);
+      } catch (err: any) {
+        setPostsLoading(false);
+        console.log(err.message);
+      }
+    };
+
+    fetchPosts();
+  }, [url]);
+
+  return { data, postsLoading };
+};
+
+export default useFetchPosts;
