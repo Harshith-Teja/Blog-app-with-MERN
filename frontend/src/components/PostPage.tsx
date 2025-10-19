a0a3dec19dcbd51d49c2a3fce1c08e308b8ad317
Harshith Teja <harshithtejadontoju@gmail.com>
2025-10-19 15:39:53 +0530

    replaced the fetchPosts() func with useFetchPosts() hook to keep the code dry; used optional chaining while displaying post properties to prevent run time errors

--- frontend/src/components/PostPage.tsx
+++ frontend/src/components/PostPage.tsx
@@ -6,73 +6,54 @@ import CommentSection from "./CommentSection";
 import PostCard from "./PostCard";
 import { PostType } from "../types/PostType";
 import { BASE_URL } from "../api/requestUrl";
+import useFetchPosts from "../hooks/fetch/useFetchPosts";
 
 const PostPage = () => {
   const { slug } = useParams();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [errMsg, setErrMsg] = useState("");
   const [post, setPost] = useState<Partial<PostType>>({});
   const [recentPosts, setRecentPosts] = useState<PostType[]>([]);
+  const {
+    postsData: mainPostData,
+    postsLoading: mainPostLoading,
+    errorMsg: mainPostErrorMsg,
+  } = useFetchPosts(`${BASE_URL}/posts/get-posts?slug=${slug}`, [slug]);
+  const { postsData: recentPostData, errorMsg: recentPostErrorMsg } =
+    useFetchPosts(`${BASE_URL}/posts/get-posts?limit=3`, []);
 
   //fetches the post on every refresh
   useEffect(() => {
-    const fetchPost = async () => {
-      setLoading(true);
-
-      try {
-        const response = await axios.get(
-          `${BASE_URL}/posts/get-posts?slug=${slug}`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        const data = response.data;
-
-        if (data.success === false) {
-          setErrMsg(data.message);
-          return;
-        }
-
-        setErrMsg("");
-        setPost(data.posts[0]);
+    const onPostFetch = async () => {
+      if (mainPostErrorMsg) {
+        setErrMsg(mainPostErrorMsg);
         setLoading(false);
-      } catch (err: any) {
-        setErrMsg(err.message);
-        setLoading(false);
+        return;
       }
+
+      setErrMsg("");
+      setPost(mainPostData?.posts[0]);
+      setLoading(mainPostLoading);
     };
 
-    fetchPost();
-  }, [slug]);
+    onPostFetch();
+  }, [slug, mainPostData]);
 
   //fetches 3 recent posts on every refresh
   useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-
-      try {
-        const response = await axios.get(
-          `${BASE_URL}/posts/get-posts?limit=3`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        const data = response.data;
-
-        if (data.success === false) {
-          return;
-        }
-
-        setRecentPosts(data.posts);
-      } catch (err: any) {
-        setErrMsg(err.message);
+    const onRecentPostFetch = async () => {
+      if (recentPostErrorMsg) {
+        setErrMsg(recentPostErrorMsg);
+        setLoading(false);
+        return;
       }
+
+      setErrMsg("");
+      setRecentPosts(recentPostData.posts);
     };
 
-    fetchPosts();
-  }, []);
+    onRecentPostFetch();
+  }, [recentPostData]);
   return (
     <>
       {loading ? (
@@ -85,33 +66,33 @@ const PostPage = () => {
             {errMsg}
           </Alert>
           <h1 className="text-3xl lg:text-4xl mt-10 p-3 text-center max-w-2xl mx-auto">
-            {post.title}
+            {post?.title}
           </h1>
           <Button color="gray" pill size="xs" className="self-center mt-5">
-            <Link to={`/search?category=${post.category}`}>
-              {post.category}
+            <Link to={`/search?category=${post?.category}`}>
+              {post?.category}
             </Link>
           </Button>
           <section className="flex justify-between p-3 border-b border-slate-500 w-full max-w-2xl mx-auto text-xs">
             <span>
-              {new Date(post.createdAt as string).toLocaleDateString()}
+              {new Date(post?.createdAt as string).toLocaleDateString()}
             </span>
             <span className="italic">
-              {post.content && (post.content.length / 1000).toFixed(0)} mins
+              {post?.content && (post?.content.length / 1000).toFixed(0)} mins
               read
             </span>
           </section>
           <section
             className="p-3 w-full max-w-3xl mx-auto post-content"
-            dangerouslySetInnerHTML={{ __html: post.content as string }}
+            dangerouslySetInnerHTML={{ __html: post?.content as string }}
           ></section>
-          <CommentSection postId={post._id as string} />
+          <CommentSection postId={post?._id as string} />
 
           <section className="flex flex-col justify-between items-center my-5">
             <h1 className="text-xl my-5">Recent articles</h1>
             <div className="flex flex-col sm:flex-row gap-6">
               {recentPosts.map((post) => (
-                <PostCard key={post._id} post={post} />
+                <PostCard key={post?._id} post={post} />
               ))}
             </div>
           </section>
