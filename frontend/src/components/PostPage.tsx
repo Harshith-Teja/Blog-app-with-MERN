9a008727a554dba24496271445e1beee77b2b364
Harshith Teja <harshithtejadontoju@gmail.com>
2025-10-27 12:01:14 +0530

    commented out unused axios package to resolve build issues

--- frontend/src/components/PostPage.tsx
+++ frontend/src/components/PostPage.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { Alert, Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
